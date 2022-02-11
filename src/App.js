04c9f80e7846cd909c2ded64e7fe06c251b1a901
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 13:13:14 +0530

    Routing- Routes and Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import UseCaseOne from './components/UseCaseOne';
 import UseCaseTwoHome from './components/UseCaseTwo/UseCaseTwoHome';
 import './App.css';
+import Navbar from './components/RoutingApp/Navbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Products from './components/RoutingApp/Products';
+import Home from './components/RoutingApp/Home';
+import Posts from './components/RoutingApp/Posts';
+import Dashboard from './components/RoutingApp/admin/Dashboard';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
 
-      <UseCaseTwoHome />
+        <Navbar />
+        <div>
+          <Switch>
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
 
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/RoutingApp/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/products">Products</a>
+            </li>
+            <li>
+                <a href="/posts/2022/02">Post</a>
+            </li>
+            <li>
+                <a href="/admin">Admin</a>
+            </li>
+        </ul>
+    )
+}
