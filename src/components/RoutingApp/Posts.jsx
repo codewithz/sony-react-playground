0ae85cfba3ecadcfe616291c859f78115b171ff8
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 15:46:00 +0530

    Routing- Query String Parameters

--- src/components/RoutingApp/Posts.jsx
+++ src/components/RoutingApp/Posts.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import queryString from 'query-string';
 
 export default function Posts(props) {
 
     const { year, month } = props.match.params;
+    const qsData = props.location.search;
+    const queryStringData = queryString.parse(qsData);
+    console.log(queryStringData);
     return (
         <div>
             <h1>Posts</h1>
