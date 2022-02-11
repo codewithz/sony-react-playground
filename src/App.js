ccc8b56af18f686bc4557c686a1f79249567a0f5
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 15:50:09 +0530

    Routing- Redirection in Routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import UseCaseOne from './components/UseCaseOne';
 import UseCaseTwoHome from './components/UseCaseTwo/UseCaseTwoHome';
 import './App.css';
 import Navbar from './components/RoutingApp/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Products from './components/RoutingApp/Products';
 import Home from './components/RoutingApp/Home';
 import Posts from './components/RoutingApp/Posts';
 import Dashboard from './components/RoutingApp/admin/Dashboard';
 import ProductDetails from './components/RoutingApp/ProductDetails';
+import NotFound from './components/RoutingApp/NotFound';
 
 function App() {
   return (
@@ -20,8 +21,10 @@ function App() {
             <Route path="/products/:id" component={ProductDetails} />
             <Route path="/products" render={(props) => <Products sortBy="newest" {...props} />} />
             <Route path="/posts/:year?/:month?" component={Posts} />
+            <Route path="/not-found" component={NotFound} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
 
