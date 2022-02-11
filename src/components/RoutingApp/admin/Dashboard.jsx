6ae80f77c15f133a1c259deb0fc2fe4e5d55714c
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 16:08:05 +0530

    Routing- Nested Routing

--- src/components/RoutingApp/admin/Dashboard.jsx
+++ src/components/RoutingApp/admin/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import AdminPosts from './AdminPosts';
+import Users from './Users';
 
 export default function Dashboard() {
     return (
         <div>
             <h1>Admin Dashboard</h1>
+            <Sidebar />
+            <div>
+                <Route path="/admin/post" component={AdminPosts} />
+                <Route path="/admin/users" component={Users} />
+            </div>
         </div>
     )
 }
--- src/components/RoutingApp/admin/Sidebar.jsx
+++ src/components/RoutingApp/admin/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
     return (
         <div>
             <ul>
-                <li>Admin Posts</li>
-                <li>Users</li>
+                <li><Link to="/admin/post">Admin Posts</Link></li>
+                <li><Link to="/admin/users">Admin Users</Link></li>
             </ul>
         </div>
     )
