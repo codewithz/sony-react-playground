796f345990e65f3ad0b6bbb692f8f388b3c1bce9
Zartab Nakhwa <zartab@codewithz.com>
2022-02-11 15:58:34 +0530

    Routing- Programmatic Navigation history props

--- src/components/RoutingApp/ProductDetails.jsx
+++ src/components/RoutingApp/ProductDetails.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 export default function ProductDetails(props) {
 
     const { id } = props.match.params;
+
+    const handleSave = () => {
+        //Navigate to /products
+        props.history.push("/products");
+        // props.history.replace("/products");
+    }
     return (
         <div>
             <h1>Product Details - {id}</h1>
-            <button>Save</button>
+            <button className="btn btn-warning btn-sm"
+                onClick={handleSave}
+            >
+
+                Save
+            </button>
         </div>
     )
 }
