25b3e7543f8fb9ebfb111db3096a3d07dc2ce241
Zartab Nakhwa <zartab@codewithz.com>
2022-02-14 16:24:56 +0530

    Http Calls- Post Page

--- src/components/HttpCallsApp/PostAPIComponent.jsx
+++ src/components/HttpCallsApp/PostAPIComponent.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PostAPIComponent() {
+
+    const [posts, setPosts] = useState([]);
+
     return (
-        <div>PostAPIComponent</div>
+        <div>
+            <button className="btn btn-primary" style={{ marginBottom: 20 }}>
+                Add
+            </button>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        posts.map(post => (
+                            <tr key={post.id}>
+                                <td>{post.title}</td>
+                                <td>
+                                    <button className="btn btn-info btn-sm">
+                                        Update
+                                    </button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-danger btn-sm">
+                                        Delete
+                                    </button>
+                                </td>
+
+                            </tr>
+                        ))
+                    }
+                </tbody>
+
+            </table>
+        </div>
     )
 }
