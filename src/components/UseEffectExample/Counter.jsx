db7a66f4108584deae63ce433f4e503dc3548a5d
Zartab Nakhwa <zartab@codewithz.com>
2022-02-14 14:48:49 +0530

    useEffect- Counter App

--- src/components/UseEffectExample/Counter.jsx
+++ src/components/UseEffectExample/Counter.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Counter() {
+
+    const [name, setName] = useState("");
+    const [count, setCount] = useState(0);
+
+    //componentDidMount, componentDidUpdate,componentWillUnmount
+    useEffect(() => {
+        document.title = `${name} have clicked ${count} times`
+    })
+
+    const handleInput = (event) => {
+        setName(event.target.value)
+    }
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+
     return (
-        <div>Counter</div>
+        <div>
+            <input type="text" onChange={handleInput} />
+            <div>
+                {name} have clicked for {count} times
+            </div>
+            <br />
+            <button className="btn btn-success" onClick={handleIncrement}>Increment</button>
+        </div>
     )
 }
